d083e2d2b274debcecdbbaa9bb395036e8df3a20
zkapornai <zkapornai@gmail.com>
2025-10-28 14:28:25 +0100

    Changes
    Moved square to bottom of screen, added left/right movement with j/l keys, and color changes on movement to reflect emotion.

--- Template/script.js
+++ Template/script.js
@@ -1,36 +1,48 @@
-import * as Util from "./util.js";
+const squareEl = document.getElementById('square');
 
-const square = document.getElementById('square');
-let squareX= 180;
-let speed = 5;
-let squareMoving = []
+// Variables
+const square = {
+  x: 600,
+  y: 620,
+  width: 50,
+  height: 50,
+  speed: 5,
+  colour: 0,
+};
 
-
-//loop function
 function loop() {
-  update();
+  update();1
   draw();
   requestAnimationFrame(loop);
 }
- 
-// future setup here
-function setup() {
-  
-}
 
-// move blocks, check collisions
+//update function for keydown
 function update() {
 
 }
 
+// draw: a square megjelenítése a DOM-on
 function draw() {
-  square.style.left = squareX + 'px';
+  squareEl.style.left = square.x + 'px';
+  squareEl.style.top = square.y + 'px';
+  squareEl.style.width = square.width + 'px';
+  squareEl.style.height = square.height + 'px';
+  squareEl.style.backgroundColor = `hsl(${square.colour}, 100%, 50%)`; // szín HSL-ben
 }
 
+// keydown wvwnt. Moving left and right for j and l. Changing colour when using this two, for emotion.
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'j') squareX -= speed;
-  if (event.key === 'l') squareX += speed;
+  if (event.key === 'j') {
+    square.x -= square.speed;
+    square.colour += 40; // színváltozás
+    if (square.colour > 360) square.colour -= 360; // körbeforgatás
+  }
+  if (event.key === 'l') {
+    square.x += square.speed;
+    square.colour += 40; // színváltozás
+    if (square.colour > 360) square.colour -= 360; // körbeforgatás
+  }
+  
 });
 
-setup();
 loop();
